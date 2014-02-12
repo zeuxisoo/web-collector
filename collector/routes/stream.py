67c20af23798edceb8ef022f036dc91a805d7cd8
Zeuxis Lo <seekstudio@gmail.com>
2014-02-12 18:31:26 +0800

    clean random code in stream detail view

--- collector/routes/stream.py
+++ collector/routes/stream.py
@@ -2,14 +2,13 @@
 
 from flask import Blueprint
 from flask import render_template
-from sqlalchemy.sql import func
-from ..models import Stream
+from ..models import db, Stream
 
 blueprint = Blueprint('stream', __name__)
 
 @blueprint.route('/image/<int:result_id>-<name>')
 def image(result_id, name):
     stream = Stream.query.filter_by(result_id=result_id).first()
-    random = Stream.query.order_by(func.random()).offset(0).limit(12).all()
+    random = Stream.query.order_by(db.func.random()).offset(0).limit(12).all()
 
     return render_template('stream/image.html', stream=stream, random=random)
