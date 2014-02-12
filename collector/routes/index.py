93d036f5af90e4ca4629ca4d0331def5ae3da170
Zeuxis Lo <seekstudio@gmail.com>
2014-02-12 18:31:55 +0800

    show today girl and total streams

--- collector/routes/index.py
+++ collector/routes/index.py
@@ -2,8 +2,8 @@
 
 import os
 from flask import Blueprint
-from flask import render_template, request, abort
-from ..models import Stream
+from flask import render_template, request, abort, current_app
+from ..models import db, Stream
 from ..helpers.value import force_integer
 
 blueprint = Blueprint('index', __name__)
@@ -15,6 +15,8 @@ def index():
     if not page:
         return abort(404)
     else:
-        paginator = Stream.query.order_by(Stream.result_created_at.desc()).paginate(page)
+        paginator  = Stream.query.order_by(Stream.result_created_at.desc()).paginate(page)
+        total_girl = Stream.query.count()
+        today_girl = current_app.curator.girl_of_the_day()
 
-        return render_template('index.html', paginator=paginator)
+        return render_template('index.html', paginator=paginator, total_girl=total_girl, today_girl=today_girl.today_result())
