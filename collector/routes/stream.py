d14ae7089d8eb6cb8a7251b37776051c5e2553fe
Zeuxis Lo <seekstudio@gmail.com>
2014-02-13 18:31:24 +0800

    fix throw error in stream detail page when user not sign in

--- collector/routes/stream.py
+++ collector/routes/stream.py
@@ -9,7 +9,8 @@ blueprint = Blueprint('stream', __name__)
 
 @blueprint.route('/<int:result_id>-<name>')
 def index(result_id, name):
-    stream = Stream.query.filter_by(result_id=result_id).first()
-    random = Stream.query.order_by(db.func.random()).offset(0).limit(12).all()
+    stream  = Stream.query.filter_by(result_id=result_id).first()
+    random  = Stream.query.order_by(db.func.random()).offset(0).limit(12).all()
+    user_id = g.user.id if g.user else None
 
-    return render_template('stream/index.html', stream=stream, random=random, bookmarked=is_bookmarked('stream', stream.id, g.user.id))
+    return render_template('stream/index.html', stream=stream, random=random, bookmarked=is_bookmarked('stream', stream.id, user_id))
