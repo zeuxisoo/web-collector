e729bbb8fccbd8be939ab719f267c798406b00e7
Zeuxis Lo <seekstudio@gmail.com>
2014-02-13 16:11:22 +0800

    move route from stream/image/id-name to stream/id-name

--- collector/routes/stream.py
+++ collector/routes/stream.py
@@ -7,9 +7,9 @@ from ..helpers.bookmark import is_bookmarked
 
 blueprint = Blueprint('stream', __name__)
 
-@blueprint.route('/image/<int:result_id>-<name>')
-def image(result_id, name):
+@blueprint.route('/<int:result_id>-<name>')
+def index(result_id, name):
     stream = Stream.query.filter_by(result_id=result_id).first()
     random = Stream.query.order_by(db.func.random()).offset(0).limit(12).all()
 
-    return render_template('stream/image.html', stream=stream, random=random, bookmarked=is_bookmarked('stream', stream.id, g.user.id))
+    return render_template('stream/index.html', stream=stream, random=random, bookmarked=is_bookmarked('stream', stream.id, g.user.id))
