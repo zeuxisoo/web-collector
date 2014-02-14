326daf28be65cec91e50875a5c701351452cdb06
Zeuxis Lo <seekstudio@gmail.com>
2014-02-14 19:27:05 +0800

    decrease random photo from 20 to 12 in stream detail view

--- collector/routes/stream.py
+++ collector/routes/stream.py
@@ -10,7 +10,7 @@ blueprint = Blueprint('stream', __name__)
 @blueprint.route('/<int:result_id>-<name>')
 def index(result_id, name):
     stream  = Stream.query.filter_by(result_id=result_id).first()
-    random  = Stream.randomly(0, 20)
+    random  = Stream.randomly(0, 12)
     user_id = g.user.id if g.user else None
 
     return render_template('stream/index.html', stream=stream, random=random, bookmarked=is_bookmarked('stream', stream.id, user_id))
