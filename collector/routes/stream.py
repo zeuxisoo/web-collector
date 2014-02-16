75bdb294220d438b6c13779ff0de3b3f1e71e95d
Zeuxis Lo <seekstudio@gmail.com>
2014-02-16 15:45:56 +0800

    add older and latest link in stream detail view

--- collector/routes/stream.py
+++ collector/routes/stream.py
@@ -13,4 +13,11 @@ def index(result_id, name):
     random  = Stream.randomly(0, 12)
     user_id = g.user.id if g.user else None
 
-    return render_template('stream/index.html', stream=stream, random=random, bookmarked=is_bookmarked('stream', stream.id, user_id))
+    old_new = Stream.query.filter(
+        db.or_(
+            Stream.result_id == Stream.query.with_entities(db.func.min(Stream.result_id).label('min')).filter(Stream.result_id > stream.result_id),
+            Stream.result_id == Stream.query.with_entities(db.func.max(Stream.result_id).label('max')).filter(Stream.result_id < stream.result_id)
+        )
+    ).order_by(Stream.result_created_at.asc()).all()
+
+    return render_template('stream/index.html', stream=stream, random=random, bookmarked=is_bookmarked('stream', stream.id, user_id), old_new=old_new)
