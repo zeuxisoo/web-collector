c96842177f1bc451d3fd35ccaabb7e50ee9857a2
Zeuxis Lo <seekstudio@gmail.com>
2014-03-03 14:29:31 +0800

    add new api for today detail

--- collector/curators/api.py
+++ collector/curators/api.py
@@ -3,12 +3,13 @@
 
 import requests
 from .stream import Stream
-from .today import Today
+from .today import Today, TodayDetail
 
 class API(object):
 
-    stream_url = "http://curator.im/api/stream/"
-    today_url  = "http://curator.im/api/girl_of_the_day/"
+    stream_url       = "http://curator.im/api/stream/"
+    today_url        = "http://curator.im/api/girl_of_the_day/"
+    today_detail_url = "http://curator.im/api/girl_of_the_day/{0}/"
 
     def __init__(self, token, format="json"):
         self.token  = token
@@ -30,3 +31,11 @@ class API(object):
         })
 
         return Today(response)
+
+    def today_detail(self, date_string):
+        response = requests.get(self.today_detail_url.format(date_string), params={
+            'token' : self.token,
+            'format': self.format,
+        })
+
+        return TodayDetail(response)
--- collector/curators/today.py
+++ collector/curators/today.py
@@ -1,5 +1,8 @@
 # coding: utf-8
 
+from math import ceil
+from decimal import Decimal
+
 class Today(object):
 
     def __init__(self, response):
@@ -7,16 +10,48 @@ class Today(object):
         self.json     = response.json()
 
     def count(self):
-        return self.json['count'],
+        return self.json['count']
 
     def next(self):
-        return self.json['next'],
+        return self.json['next']
 
     def previous(self):
-        return self.json['previous'],
+        return self.json['previous']
 
     def results(self):
         return self.json['results']
 
+    def total_results(self):
+        return len(self.results())
+
+    def total_pages(self):
+        return int(ceil(self.count() / Decimal(self.total_results())))
+
+    def find_result_by_id(self, id_):
+        for result in self.results():
+            if result['id'] == int(id_):
+                return result
+
+        return None
+
     def today_result(self):
         return self.results()[0]
+
+class TodayDetail(object):
+
+    def __init__(self, response):
+        self.response = response
+        self.json     = response.json()
+
+    def results(self):
+        return self.json
+
+    def total_results(self):
+        return len(self.results())
+
+    def find_result_by_id(self, id_):
+        for result in self.results():
+            if result['id'] == int(id_):
+                return result
+
+        return None
