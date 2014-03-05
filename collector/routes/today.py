4c86f240517a5dde753bc712915d9e4d34c560ad
Zeuxis Lo <seekstudio@gmail.com>
2014-03-05 15:14:37 +0800

    display today girl in today page

--- collector/routes/today.py
+++ collector/routes/today.py
@@ -2,12 +2,19 @@
 
 import os
 from flask import Blueprint
-from flask import render_template, request, abort, current_app
-from ..models import db, Stream
+from flask import render_template, request, abort
+from ..models import db, Today
 from ..helpers.value import force_integer
 
 blueprint = Blueprint('today', __name__)
 
 @blueprint.route('/')
 def index():
-    return render_template('today/index.html')
+    page = force_integer(request.args.get('page', 1), 0)
+
+    if not page:
+        return abort(404)
+    else:
+        paginator  = Today.query.order_by(Today.result_date.desc()).paginate(page)
+
+        return render_template('today/index.html', paginator=paginator)
