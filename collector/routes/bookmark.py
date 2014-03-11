f6f9cd90694f1955f961fa00f55e9ea56fb22d65
Zeuxis Lo <seekstudio@gmail.com>
2014-03-11 16:18:14 +0800

    show today category in bookmark page

--- collector/routes/bookmark.py
+++ collector/routes/bookmark.py
@@ -9,35 +9,56 @@ from ..helpers.bookmark import is_bookmarked
 
 blueprint = Blueprint('bookmark', __name__)
 
-@blueprint.route('/')
+@blueprint.route('/', defaults={ 'category': 'stream' })
+@blueprint.route('/<category>')
 @require_login
-def index():
+def index(category):
     page = force_integer(request.args.get('page', 1), 0)
 
     if not page:
         return abort(404)
     else:
-        paginator = Stream.query.outerjoin(Bookmark, Bookmark.target_id == Stream.result_id).filter(
-            Bookmark.category == 'stream',
-            Bookmark.user_id == g.user.id,
-        ).order_by(Bookmark.create_at.desc()).paginate(page)
+        if category == 'stream':
+            paginator = Stream.query.outerjoin(Bookmark, Bookmark.target_id == Stream.result_id).filter(
+                Bookmark.category == 'stream',
+                Bookmark.user_id == g.user.id,
+            ).order_by(Bookmark.create_at.desc()).paginate(page)
 
-        total_bookmark = Bookmark.query.filter_by(category='stream', user_id=g.user.id).count()
-        random_stream  = Stream.randomly(0, 6)
+            total_bookmark = Bookmark.query.filter_by(category='stream', user_id=g.user.id).count()
+            random_images  = Stream.randomly(0, 6)
+        elif category == 'today':
+            paginator = Today.query.outerjoin(Bookmark, Bookmark.target_id == Today.result_id).filter(
+                Bookmark.category == 'today',
+                Bookmark.user_id == g.user.id,
+            ).order_by(Bookmark.create_at.desc()).paginate(page)
 
-        return render_template('bookmark/index.html', paginator=paginator, total_bookmark=total_bookmark, random_stream=random_stream)
+            total_bookmark = Bookmark.query.filter_by(category='today', user_id=g.user.id).count()
+            random_images  = Today.randomly(0, 6)
 
-@blueprint.route('/detail/<int:result_id>-<name>')
-def detail(result_id, name):
-    stream  = Stream.query.filter_by(result_id=result_id).first()
-    random  = Stream.randomly(0, 12)
+        return render_template('bookmark/index.html', paginator=paginator, total_bookmark=total_bookmark, random_images=random_images, category=category)
+
+@blueprint.route('/detail/<category>/<int:result_id>-<name>')
+def detail(category, result_id, name):
     user_id = g.user.id if g.user else None
 
-    # Find user bookmarks, mark it is subquery, temp table
-    user_bookmarks_subquery = Stream.query.outerjoin(Bookmark, Bookmark.target_id == Stream.id).filter(
-        Bookmark.category == 'stream',
-        Bookmark.user_id == 1,
-    ).order_by(Bookmark.create_at.desc()).subquery()
+    if category == 'stream':
+        model  = Stream.query.filter_by(result_id=result_id).first()
+        random = Stream.randomly(0, 12)
+
+        # Find user bookmarks, mark it is subquery, temp table
+        user_bookmarks_subquery = Stream.query.outerjoin(Bookmark, Bookmark.target_id == Stream.result_id).filter(
+            Bookmark.category == 'stream',
+            Bookmark.user_id == user_id,
+        ).order_by(Bookmark.create_at.desc()).subquery()
+    elif category == 'today':
+        model  = Today.query.filter_by(result_id=result_id).first()
+        random = Today.randomly(0, 12)
+
+        # Find user bookmarks, mark it is subquery, temp table
+        user_bookmarks_subquery = Today.query.outerjoin(Bookmark, Bookmark.target_id == Today.result_id).filter(
+            Bookmark.category == 'today',
+            Bookmark.user_id == user_id,
+        ).order_by(Bookmark.create_at.desc()).subquery()
 
     # Set query object in the temp table user_bookmark_subquery (step 1 marked)
     user_bookmarks_query = db.session.query(user_bookmarks_subquery)
@@ -45,12 +66,12 @@ def detail(result_id, name):
     # Find out next and previous record in query object user_bookmarks_query (step 2 marked)
     old_new = user_bookmarks_query.filter(
         db.or_(
-            user_bookmarks_subquery.c.result_id == user_bookmarks_query.with_entities(db.func.min(user_bookmarks_subquery.c.result_id).label('min')).filter(user_bookmarks_subquery.c.result_id > stream.result_id),
-            user_bookmarks_subquery.c.result_id == user_bookmarks_query.with_entities(db.func.max(user_bookmarks_subquery.c.result_id).label('max')).filter(user_bookmarks_subquery.c.result_id < stream.result_id)
+            user_bookmarks_subquery.c.result_id == user_bookmarks_query.with_entities(db.func.min(user_bookmarks_subquery.c.result_id).label('min')).filter(user_bookmarks_subquery.c.result_id > model.result_id),
+            user_bookmarks_subquery.c.result_id == user_bookmarks_query.with_entities(db.func.max(user_bookmarks_subquery.c.result_id).label('max')).filter(user_bookmarks_subquery.c.result_id < model.result_id)
         )
     ).all()
 
-    return render_template('bookmark/detail.html', stream=stream, random=random, bookmarked=is_bookmarked('stream', stream.id, user_id), old_new=old_new)
+    return render_template('bookmark/detail.html', model=model, random=random, bookmarked=is_bookmarked(category, model.result_id, user_id), old_new=old_new, category=category)
 
 @blueprint.route('/create/stream/<int:result_id>')
 @require_login
