091e1db172aae2f468cf78b121a170551dcebcdc
Zeuxis Lo <seekstudio@gmail.com>
2014-03-13 15:14:36 +0800

    add href in stream today girl and change size from 240 to 230 width

--- collector/routes/ajax.py
+++ collector/routes/ajax.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from flask import Blueprint
-from flask import current_app, jsonify
+from flask import current_app, jsonify, url_for
 from ..filters import Embedly
 
 blueprint = Blueprint('ajax', __name__)
@@ -12,6 +12,7 @@ def today_girl():
     today_result   = today_girl.today_result()
     embedly_filter = Embedly(current_app.config.get('EMBEDLY_API_TOKEN'))
 
-    today_result['image'] = embedly_filter.fill(today_result['image'], 240, 240, 'FFF', True)
+    today_result['image'] = embedly_filter.fill(today_result['image'], 230, 230, 'FFF', True)
+    today_result['href']  = url_for('today.detail', result_date=today_result['date'], result_id=today_result['id'], name=today_result['name'])
 
     return jsonify(today_result)
