7378592048f9a7813874cac5fec80b3707186810
Zeuxis Lo <seekstudio@gmail.com>
2014-03-23 15:32:01 +0800

    improve 2 helper.value method when using filter_in

--- collector/helpers/value.py
+++ collector/helpers/value.py
@@ -10,7 +10,7 @@ def force_integer(value, default=1):
 
 def fill_with_users(items):
     user_ids  = [item.user_id for item in items]
-    users     = User.query.filter(User.id.in_(user_ids)).all()
+    users     = User.query.filter(User.id.in_(user_ids)).all() if user_ids else {}
     user_dict = {user.id: user for user in users}
 
     for item in items:
@@ -22,8 +22,8 @@ def fill_with_images(items):
     stream_target_ids = [item.target_id for item in items if item.category == 'stream']
     today_target_ids  = [item.target_id for item in items if item.category == 'today']
 
-    streams = Stream.query.filter(Stream.result_id.in_(stream_target_ids)).all()
-    todays  = Today.query.filter(Today.result_id.in_(today_target_ids)).all()
+    streams = Stream.query.filter(Stream.result_id.in_(stream_target_ids)).all() if stream_target_ids else {}
+    todays  = Today.query.filter(Today.result_id.in_(today_target_ids)).all() if today_target_ids else {}
 
     stream_dict = { stream.result_id: stream for stream in streams }
     today_dict  = { today.result_id: today for today in todays }
