63d3c35ff14c6fdf7ad2c32f6aed9ae2b4388992
Zeuxis Lo <seekstudio@gmail.com>
2014-03-24 15:19:43 +0800

    using task logger replace print method in task.dropbox

--- collector/tasks/dropbox.py
+++ collector/tasks/dropbox.py
@@ -4,20 +4,23 @@ from __future__ import absolute_import
 import os
 from flask import g
 from celery import task
+from celery.utils.log import get_task_logger
 from dropbox import client
 from ..models import UserConnection, DropboxLog
 from ..helpers.task import download_image
 
+logger = get_task_logger(__name__)
+
 @task
 def sync_image(category, user_id, result_id):
-    print("called dropbox.save_to_dropbox, category: {0}, result id: {1}".format(category, result_id))
+    logger.info("called dropbox.save_to_dropbox, category: {0}, result id: {1}".format(category, result_id))
 
     saved_file      = download_image(category, result_id)
     user_connection = UserConnection.query.filter_by(user_id=user_id, provider_name='dropbox').first()
     status          = 'faield'
 
-    print("==> saved_file: {0}".format(saved_file))
-    print("==> user_connection: {0}".format('PASS' if user_connection.access_token != '' else 'Failed'))
+    logger.info("==> saved_file: {0}".format(saved_file))
+    logger.info("==> user_connection: {0}".format('PASS' if user_connection.access_token != '' else 'Failed'))
 
     if saved_file and user_connection and user_connection.access_token != '':
         dropbox_client = client.DropboxClient(user_connection.access_token)
@@ -30,9 +33,9 @@ def sync_image(category, user_id, result_id):
 
         status = 'success'
 
-        print("==> Saved")
+        logger.info("==> Saved")
     else:
-        print("==> Failed")
+        logger.info("==> Failed")
 
     DropboxLog(
         category  = category,
