a752d81c2a21b8b879cb68384768133f298e7176
Zeuxis Lo <seekstudio@gmail.com>
2014-07-20 02:10:56 +0800

    fix ajax request crash in archive mode

--- collector/routes/ajax.py
+++ collector/routes/ajax.py
@@ -12,7 +12,7 @@ def today_girl():
     today_result   = today_girl.today_result()
     embedly_filter = Embedly(current_app.config.get('EMBEDLY_API_TOKEN'))
 
-    today_result['image'] = embedly_filter.fill(today_result['image'], 230, 230, 'FFF', True)
+    today_result['image'] = embedly_filter.fill('stream', today_result['image'], 230, 230, 'FFF', True)
     today_result['href']  = url_for('today.detail', result_date=today_result['date'], result_id=today_result['id'], name=today_result['name'])
 
     return jsonify(today_result)
